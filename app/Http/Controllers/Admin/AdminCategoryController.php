f5e021b34a667c690a72d35d68cd18fb061984c9
kuzma17 <v.kuzma@mail.ru>
2023-10-05 09:33:50 +0300

    add Admin panel

---
+++ app/Http/Controllers/Admin/AdminCategoryController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
+use App\Models\Category;
+use App\Services\ImageService;
+use Illuminate\Http\Request;
+
+class AdminCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::sortDesc()->get();
+        return view('admin.categories.index', ['categories' => $categories]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoryRequest $request, ImageService $imageService)
+    {
+        if (!$request->parent_id){
+            $request['parent_id'] = 0;
+        }
+        $image = $request->files->get('file');
+        if ($image){
+            $name = $request->slug.'.jpg';
+            $request['image'] = $imageService->create($image, $name, 290, 290);
+        }
+
+        Category::create($request->all());
+
+        return redirect(route('admin.categories.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category $category)
+    {
+        return view('admin.categories.edit', ['category' => $category]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CategoryRequest $request, Category $category, ImageService $imageService)
+    {
+
+        $image = $request->files->get('file');
+        if ($image){
+            $name = $request->slug.'.jpg';
+            $request['image'] = $imageService->create($image, $name, 290, 290);
+        }
+
+        $category->update($request->all());
+
+        return redirect(route('admin.categories.index'));
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return redirect(route('admin.categories.index'));
+    }
+}
---
+++ app/Http/Controllers/Admin/AdminGoodController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\GoodRequest;
+use App\Models\Category;
+use App\Models\Good;
+use App\Services\AdminFiterGoodsService;
+use App\Services\ImageService;
+use Illuminate\Http\Request;
+use Image;
+
+class AdminGoodController extends Controller
+{
+    protected $imageService;
+
+    public function __construct(ImageService $imageService)
+    {
+        $this->imageService = $imageService;
+        $this->imageService->path(env('GOOD_PHOTO_PATH'));
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request, AdminFiterGoodsService $fiterGoodsService)
+    {
+
+
+
+        $query = Good::query();
+
+        $query = $fiterGoodsService->apply($query);
+
+        $goods = $query->sortDesc()
+            ->get()
+            ->paginate(12)
+            ->load('category', 'photos');
+
+        return view('admin.goods.index', ['goods' => $goods]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(ImageService $imageService)
+    {
+        $categories = Category::all();
+        return view('admin.goods.create', ['categories' => $categories]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(GoodRequest $request)
+    {
+        $good = Good::create($request->all());
+        $images = $request->get('images');
+
+        if ($images){
+            array_map(function ($image){
+                $this->imageService->createGoodPhotos($image['src']);
+            }, $images);
+
+            $good->photos()->createMany($images);
+        }
+
+        return redirect(route('admin.goods.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Good $good)
+    {
+        $good = $good->load('photos');
+        $categories = Category::all();
+        return view('admin.goods.edit', ['good' => $good,'categories' => $categories]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(GoodRequest $request, Good $good)
+    {
+        if(!$request->active){
+            $request['active'] = 0;
+        }
+        $good->update($request->all());
+        $images = $request->get('images');
+
+        if ($images){
+            array_map(function ($image){
+                $this->imageService->createGoodPhotos($image['src']);
+            }, $images);
+
+            $good->photos()->delete();
+            $good->photos()->createMany($images);
+        }
+
+        return redirect(route('admin.goods.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Good $good)
+    {
+        $photos = $good->photos;
+
+        if ($photos){
+            $photos->map(function ($photo){
+                $this->imageService->delete($photo->src);
+            });
+        }
+        $good->photos()->delete();
+        $good->delete();
+
+        return redirect(route('admin.goods.index'));
+    }
+}
