720a275e5743c5308cc2d88e658fab66a0703d3c
kuzma17 <v.kuzma@mail.ru>
2023-10-05 17:04:35 +0300

    echo GB

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
+{{dd(gd_info())}}
     <div id="app">
         @include('layouts.preloader')
         @include('layouts.header')
