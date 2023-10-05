b579269937c6d48f2535b5ad3910706fcdaac3e9
kuzma17 <v.kuzma@mail.ru>
2023-10-05 14:23:17 +0300

    mix

---
+++ public/js/resources_lang_ua_json.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_lang_ua_json"],{
+
+/***/ "./resources/lang/ua.json":
+/*!********************************!*\
+  !*** ./resources/lang/ua.json ***!
+  \********************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"good_name":"Найменування","code":"Код","good_count":"Кількість","price":"Ціна","good_sum":"Сума","curr":"грн.","delete_good":"удалити товар","in_cart_item":"У кошику товарів","in_sum":"на суму","clean_cart":"очистити кошик","contacts_data":"Контактні дані","user_name":"ПІБ","phone":"Телефон","nev_client":"Я новий покупець","have_account":"У мене є обліковий запис","register":"Реєстрація","register_info":"Реєстрація дозволяє зайти в особистий кабінет, відстежувати свої покупки","password":"Пароль","forgot_password":"Забули пароль","delivery":"Доставка","address":"Адреса","payment":"Спосіб оплати","login":"Вхід","personal_area":"Кабінет","remember":"Запам`ятати мене"}');
+
+/***/ })
+
+}]);
