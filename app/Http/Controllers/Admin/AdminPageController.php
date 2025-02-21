ec4b81dc21873643a597db8662eeae03508e8876
kuzma <v.kuzma@mail.ru>
2025-02-21 13:59:15 +0200

    sort in admin page

--- app/Http/Controllers/Admin/AdminPageController.php
+++ app/Http/Controllers/Admin/AdminPageController.php
@@ -14,7 +14,7 @@ class AdminPageController extends Controller
      */
     public function index()
     {
-        $pages = Page::orderBy('name_ru')->get();
+        $pages = Page::orderBy('slug')->get();
 
         return view('admin.pages.index', ['pages' => $pages]);
     }
