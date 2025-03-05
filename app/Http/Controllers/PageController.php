3019e876eecd2bbc9445a8f7a1bb900e3ee6dd9a
kuzma <v.kuzma@mail.ru>
2025-03-05 12:15:52 +0200

    test

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Page;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class PageController extends Controller
 {
@@ -18,5 +19,32 @@ class PageController extends Controller
 
     public function admin(){
         return view('admin.layouts.app');
+    }
+
+    public function store(Request $request)
+    {
+        $account_id = 106157;
+
+
+        $request->validate([
+            'name'  => 'required|string|max:255',
+            'phone' => 'required|string|max:20',
+        ]);
+
+        $name  = $request->input('name');
+        $phone = $request->input('phone');
+
+        // Отправляем данные в Chatwoot через API
+        $response = Http::post(env('CHATWOOT_API_URL') . '/api/v1/accounts/{account_id}/conversations', [
+            'inbox_id'  => env('CHATWOOT_INBOX_ID'),
+            'source_id' => $phone, // Используем телефон как идентификатор
+            'contact'   => [
+                'name'  => $name,
+                'phone' => $phone,
+            ],
+            'message' => "Запрос на обратный звонок от {$name}, телефон: {$phone}",
+        ]);
+
+        return response()->json(['message' => 'Заявка отправлена'], 201);
     }
 }
