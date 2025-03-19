df76f913f07ea6bb75b3dd7edaa513600ce1d12a
kuzma <v.kuzma@mail.ru>
2025-03-19 18:57:45 +0200

    security search throttle:5,1

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::group(['prefix' => App\Http\Middleware\Localization::getLocaleUrl()], fun
     Route::post('/login', [\App\Http\Controllers\Auth\LoginController::class, 'loginPhone'])->name('login.phone'); //Login Phone
     Route::post('/login_order', [\App\Http\Controllers\Auth\LoginController::class, 'loginOrder'])->name('login.order'); //Login from Order
 
-    Route::get('/catalog/search', [\App\Http\Controllers\SearchController::class, 'search'])->name('search');
+    Route::get('/catalog/search', [\App\Http\Controllers\SearchController::class, 'search'])->middleware('throttle:5,1')->name('search');
 
     Route::get('/catalog/{slug}', [\App\Http\Controllers\CatalogController::class, 'list'])->name('catalog');
 
