18e4fc88d05bb2fd8febd54f9d2b084451e5e27e
kuzma <v.kuzma@mail.ru>
2025-04-15 11:13:36 +0300

    fix css

--- resources/views/components/office-list.blade.php
+++ resources/views/components/office-list.blade.php
@@ -6,12 +6,19 @@
                 <i class="fa-solid fa-chevron-down"></i> {{$office->name}}
             </div>
             <div class="phone">
+                <a href="tel:{{$office->phone}}">
+                    <i class="fa-solid fa-phone"></i> {{$office->phone}}
+                </a>
+            </div>
+            <div class="phone">
                 <a href="viber://chat?number={{$office->phone}}">
                     <i class="fa-brands fa-viber"></i> {{$office->phone}}
                 </a>
             </div>
             <div>
-                <i class="fa-solid fa-location-dot"></i> {{$office->description}}
+                <a href="{{route('page', 'contacts').'#office_'.$office->id}}">
+                    <i class="fa-solid fa-location-dot"></i> {{$office->description}}
+                </a>
             </div>
 
         </div>
