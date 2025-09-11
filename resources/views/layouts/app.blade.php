920d8f4a6c650902c45bbf84a6c9583eca721e79
kuzma <v.kuzma@mail.ru>
2025-09-12 00:03:55 +0300

    googletagmanager update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,15 +12,27 @@
     <meta name="keywords" content="@yield('keywords', __('seo.site_keywords'))">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 {{--    <link href="https://cdn.jsdelivr.net/gh/Alaev-Co/snowflakes/dist/snow.min.css" rel="stylesheet">--}}
+
     <!-- Google Tag Manager -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WBRRP3QC"
-                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WBRRP3QC');</script>
+{{--    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WBRRP3QC"--}}
+{{--                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>--}}
+{{--    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':--}}
+{{--                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],--}}
+{{--            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=--}}
+{{--            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);--}}
+{{--        })(window,document,'script','dataLayer','GTM-WBRRP3QC');</script>--}}
     <!-- End Google Tag Manager -->
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17046141229"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'AW-17046141229');
+    </script>
+
 </head>
 <body>
     <div id="app">
